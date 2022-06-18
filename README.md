c1b69ce32e842cbb99887102b03c3bbce16d8b49
Thomas <t.green01q@gmail.com>
2022-06-18 17:02:20 +0100

    get random cat pic & delete messages command

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # reddit image grabbing discord bot
-very inconsistent, but works by grabbing the image tag on your subreddit of choice.
-## command
-!r subreddit_name
 
+very inconsistent, but works by grabbing the image tag on your subreddit of choice.
+
+## command
+
+!cat
+!r subreddit_name
+!delete AMOUNT
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from bs4 import BeautifulSoup
 import requests
+import urllib.request, json
 import time
 
 intents = discord.Intents.default()
@@ -16,14 +17,36 @@ class Bot(discord.Client):
 
         prefix = "!"
         if message.content == prefix + "help":
-            embedVar = discord.Embed(title="commands", color=0xC200D8)
+            embedVar = discord.Embed(title="Commands", color=0xC200D8)
             embedVar.add_field(
-                name="Retrieve top subreddit posts",
+                name="Random Cat Image",
+                value="!cat",
+                inline=False,
+            )
+            embedVar.add_field(
+                name="Retrieve Top Subreddit Posts",
                 value="!r subreddit_name",
+                inline=False,
+            )
+            embedVar.add_field(
+                name="Delete Messages",
+                value="!delete AMOUNT",
                 inline=False,
             )
             await message.channel.send(embed=embedVar)
 
+        # get image of cat
+        if message.content == prefix + "cat":
+            with urllib.request.urlopen("https://cataas.com/cat?json=true") as url:
+                data = json.loads(url.read().decode())
+                catUrl = data["url"]
+                catImage = "https://cataas.com/" + catUrl
+                embedVar = discord.Embed(title="cat", color=0xC200D8)
+                embedVar.set_image(url=catImage)
+                await message.channel.send(embed=embedVar)
+                print(catUrl)
+
+        # very hit and miss, sometimes it works at retrieving images
         reddit_prefix = "!r "
         str = message.content.lstrip("!r ")
         if message.content == reddit_prefix + str:
@@ -31,32 +54,49 @@ class Bot(discord.Client):
             response = requests.get(url)
             soup = BeautifulSoup(response.content, "html.parser")
             image_tags = soup.find_all("img", attrs={"alt": "Post image"})
+            print("requested: r/" + str)
 
-            count = 0
-            while not image_tags:
-                top = "https://www.reddit.com/r/" + str + "/top/"
-                url = top
-                response = requests.get(url)
-                soup = BeautifulSoup(response.content, "html.parser")
-                image_tags = soup.find_all("img", attrs={"alt": "Post image"})
-                time.sleep(2)
-                count += 1
+            if not image_tags:
+                await message.channel.send(
+                    "`Could not retrieve image(s) from r/"
+                    + str
+                    + "`"
+                    + "\n`Try Again!`"
+                )
+                print("Failed to retreive image(s)")
+            else:
+                for counter, image_tag in enumerate(image_tags):
+                    image_url = image_tag["src"]
+                    response = requests.get(image_url)
+                    embedVar = discord.Embed(title="r/" + str, color=0xC200D8)
+                    embedVar.set_image(url=image_url)
+                    await message.channel.send(embed=embedVar)
+                    print("retrieved " + image_url)
 
-                if count == 5:
+        deleteAmount = message.content.lstrip("!delete ")
+        try:
+            if message.content == prefix + "delete " + deleteAmount:
+                amount = int(deleteAmount)
+                if amount > 100:
+                    await message.channel.send("`Failed to delete: Maximum 100`")
+                    print("failed to delete", amount, "messages")
+                elif amount < 1:
                     await message.channel.send(
-                        "`Could not retrieve image from r/"
-                        + str
-                        + "`"
-                        + "\n`Try Again!`"
+                        "`Failed to delete: number must be greater than 0`"
                     )
-                    break
                 else:
-                    for counter, image_tag in enumerate(image_tags):
-                        image_url = image_tag["src"]
-                        response = requests.get(image_url)
-                        embedVar = discord.Embed()
-                        embedVar.set_image(url=image_url)
-                        await message.channel.send(embed=embedVar)
+                    await message.channel.purge(limit=amount)
+                    print("deleted", amount, "messages")
+                    # time.sleep(1)
+                    embedVar = discord.Embed(
+                        title="deleted",
+                        description=deleteAmount + " messages",
+                        color=0xC200D8,
+                    )
+                    await message.channel.send(embed=embedVar)
+        except:
+            print("error")
+            await message.channel.send("`!delete [Must be an integer]`")
 
 
 client = Bot(intents=intents)
